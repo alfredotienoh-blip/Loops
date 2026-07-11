c255178d5fdf7f7a9def0e1ebb169555f53b5f35
Alfred <alfred.otienoh@gmail.com>
2026-07-11 18:34:18 +0300

    Add break statement

--- for-loops.py
+++ for-loops.py
@@ -6,3 +6,12 @@ for x in fruits:
     
     for x in "banana":
         print(x)
+        
+# The Break statement
+# With break statement you can stop the loop before it has looped through all the items
+#Exit the loop when x is "banana"
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
